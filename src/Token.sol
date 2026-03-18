bf619221d0dbb8721bd6d904506f225afa1b056b
Ivan Hrekov <iandrc9@gmail.com>
2026-03-18 21:15:24 +0100

    feat: implement using OpenZeppelin ERC20

--- src/Token.sol
+++ src/Token.sol
@@ -2,23 +2,10 @@
 
 pragma solidity ^0.8.33;
 
-contract Token {
-    mapping(address => uint256) private s_balances;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    string public constant name = "My Token";
-    uint8 public constant decimals = 18;
-    uint256 public constant totalSupply = 100 ether;
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return s_balances[_owner];
+contract Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Token", "TKN") {
+        _mint(msg.sender, initialSupply);
     }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
-        s_balances[msg.sender] -= value;
-        s_balances[_to] += value;
-
-        return (balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
-    }
-
 }
